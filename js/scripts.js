087943defecf963b79f593457ebe99fe124cdbb6
nicor423 <38299316+nicor423@users.noreply.github.com>
2018-07-30 18:27:09 -0300

    prueba responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
+$(document).ready(function() {
+
+    // setTimeout(function() {
+		var element = document.getElementById("hi");
+	    element.classList.add("animated");
+	    element.classList.add("bounceIn");
+
+    // }, 1000);
+
+
+
+});
+
+$("#view_more").click(function(event) {
+	alert("hola");
+});
+
+// $("#view_more").hover(function(){
+// 	var element = document.getElementById("view_more");
+//     element.classList.add("zoom");
+// });
+// $(window).load(function() {
+//   // executes when complete page is fully loaded, including all frames, objects and images
+//   console.log("window is loaded");
+// });
+
+
 // $("#view_more").mouseover(function(event) {
 // 	addClass('animate bounce infinite');
 // });
